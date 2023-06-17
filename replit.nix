f826d72da2356f8fa1f93dfe220360bb7052deb9
JimmyHogerty <3432079-JimmyHogerty@users.noreply.replit.com>
2023-06-17 20:20:31 +0000

    This is my first commit

--- replit.nix
+++ replit.nix
@@ -1,6 +1,7 @@
 { pkgs }: {
   deps = [
-		pkgs.nodePackages.prettier
+		pkgs.gh
+  pkgs.nodePackages.prettier
     pkgs.nodejs-16_x
     pkgs.nodePackages.vscode-langservers-extracted
     pkgs.nodePackages.typescript-language-server
